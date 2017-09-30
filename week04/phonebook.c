da0a8dd13903a2de2fe4f94d531faaf4e91d5a1d
tuannd <tuannd@ows.com.vn>
2017-09-30 17:57:22 +0700

    Change phonebook into a menu app

--- week04/phonebook.c
+++ week04/phonebook.c
@@ -3,15 +3,11 @@
 #include <string.h>
 #include "Libfdr/jrb.h"
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Syntax: ./phonebook filename\n");
-        return 1;
-    }
+int main() {
     FILE *fp;
-    fp = fopen(argv[1], "r");
+    fp = fopen("phonebook.txt", "r");
     if (fp == NULL) {
-        printf("Can not open file %s\n", argv[1]);
+        printf("Can not open file %s\n", "phonebook.txt");
         return 1;
     }
 
@@ -26,22 +22,103 @@ int main(int argc, char *argv[]) {
         fgets(buff, sizeof(buff), fp);
         buff[strcspn(buff, "\n")] = 0;
         strcpy(name, buff);
-        // printf("%s\n", name);
 
         fgets(buff, sizeof(buff), fp);
         buff[strcspn(buff, "\n")] = 0;
         strcpy(number, buff);
-        // printf("%s\n", number);
-        jrb_insert_str(book, strdup(name), 
-        new_jval_l(atol(number)));
+        jrb_insert_str(book, strdup(name), new_jval_l(atol(number)));
         i++;
     }
 
-    jrb_traverse(node, book) {
-        printf("%s\t\t%ld\n", jval_s(node->key), jval_l(node->val));
+    fclose(fp);
+
+    // Menu
+    int menu = 0;
+    while (menu != -1) {
+        printf("\t--Phonebook menu using JRB--\n");
+        printf("1. Print current phonebook\n");
+        printf("2. Add/modify an entry\n");
+        printf("3. Delete an entry\n");
+        printf("4. Save to file\n");
+        printf("5. Exit\n");
+        printf("Selection: ");
+        scanf("%d", &menu);
+        switch(menu) {
+            case 1:
+                jrb_traverse(node, book) {
+                    printf("%s\t%ld\n", jval_s(node->key), jval_l(node->val));
+                }
+                break;
+            case 2:
+                printf("Name: ");
+                while(getchar()!='\n');
+                scanf("%[^\n]", name);
+                printf("Number: ");
+                while(getchar()!='\n');
+                scanf("%s", number);
+
+                node = jrb_find_str(book, name);
+                if (node != NULL) {
+                    // Entry already existed
+                    jrb_delete_node(node);
+                    printf("Modified entry %s\n", name);
+                } else {
+                    printf("Added new entry %s\n", name);
+                }
+
+                jrb_insert_str(book, strdup(name), new_jval_l(atol(number)));
+                break;
+            case 3:
+                printf("Name: ");
+                while(getchar()!='\n');
+                scanf("%[^\n]", name);
+
+                node = jrb_find_str(book, name);
+                if (node != NULL) {
+                    jrb_delete_node(node);
+                    printf("Deleted entry %s\n", name);
+                } else {
+                    printf("Entry %s not found\n", name);
+                }
+                break;
+            case 4:
+                fp = fopen("phonebook.txt", "w");
+                i = 1;
+                jrb_traverse(node, book) {
+                    if (i > 1) fprintf(fp, "\n");
+                    fprintf(fp, "%s\n", jval_s(node->key));
+                    fprintf(fp, "%ld", jval_l(node->val));
+                    i++;
+                    // This usage of fprintf instead of
+                    // fprintf(fp, "%s\n", jval_s(node->key));
+                    // fprintf(fp, "%ld\n", jval_l(node->val));
+                    // ensure that there is no extra line at
+                    // the end of the file
+                }
+                printf("Write to file completed\n");
+                fclose(fp);
+                break;
+            case 5:
+                menu = -1;
+                break;
+            default:
+                menu = 0;
+                printf("Invalid choice\n");
+                break;
+        }
     }
 
-    fclose(fp);
+    node = jrb_find_str(book, "Nguyen Dinh Tuan");
+    if (node != NULL) {
+        jrb_delete_node(node);
+        strcpy(name, "Nguyen Dinh Tuan");
+        strcpy(number, "123123123");
+        jrb_insert_str(book, strdup(name), new_jval_l(atol(number)));
+    } else {
+        printf("Not found\n");
+    }
+
+    jrb_free_tree(book);
 
     return 0;
 }
