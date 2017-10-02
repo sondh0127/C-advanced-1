4790f20a986162d0ddb26b28bc5f7902418b047f
tuannd <tuannd@ows.com.vn>
2017-10-02 20:17:20 +0700

    Finish phonebook base on B-Tree

--- week05/B-Tree/phonebook.c
+++ week05/B-Tree/phonebook.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "bt/inc/btree.h"
 
 int main(int argc, char *argv[]) {
@@ -9,51 +10,63 @@ int main(int argc, char *argv[]) {
     BTA *btfile;
     btinit();
 
-    char name[50] = "Nguyen Dinh Tuan";
-    long number = 123123123;
-
     // Open bt file
     btfile = btopn(argv[1], 0, FALSE);
     if (btfile == NULL) {
         // If the file doesn't exist, create a new one
         btfile = btcrt(argv[1], 0, FALSE);
         printf("Can not find file %s, creating a new one.\n", argv[1]);
-        // binsky(btfile, name, number);
-    } else {
-        //print
-        binsky(btfile, name, number);
     }
 
-    int val;
-    // Return 0 if founded, non-zero if not founded
-    printf("Found: %d\n", bfndky(btfile, name, &val));
-    printf("%d\n", val);
-
     int menu = 0;
+    char name[50];
+    char number[20];
+    int val;
     while (menu != -1) {
         printf("\t--Phonebook application using B-Tree--\n");
         printf("1. Print all entries\n");
         printf("2. Add/modify an entry\n");
         printf("3. Delete an entry\n");
-        printf("4. Save to file\n");
-        printf("5. Exit\n");
+        printf("4. Save and exit\n");
         printf("Selection: ");
         scanf("%d", &menu);
         switch(menu) {
             case 1:
-                
+                // First position in the bt tree
+                btpos(btfile, ZSTART);
+                while(bnxtky(btfile, name, &val) == 0) {
+                    printf("%s\t\t%d\n", name, val);
+                }
                 break;
             case 2:
-                //add/modify
+                printf("Name: ");
+                while(getchar()!='\n');
+                scanf("%[^\n]", name);
+                printf("Number: ");
+                scanf("%s", number);
+                if (bfndky(btfile, name, &val) == 0) {
+                    bupdky(btfile, name, atoi(number));
+                    printf("Modified entry %s\n", name);
+                } else {
+                    binsky(btfile, name, atoi(number));
+                    printf("Added entry %s\n", name);
+                }
                 break;
             case 3:
-                //delete
+                printf("Name: ");
+                while(getchar()!='\n');
+                scanf("%[^\n]", name);
+                if (bfndky(btfile, name, &val) == 0) {
+                    bdelky(btfile, name);
+                    printf("Deleted entry %s\n", name);
+                } else {
+                    printf("Entry %s does not exists.\n", name);
+                }
                 break;
             case 4:
-                //write
-                break;
-            case 5:
                 menu = -1;
+                // Close bt file
+                btcls(btfile);
                 break;
             default:
                 menu = 0;
@@ -61,8 +74,6 @@ int main(int argc, char *argv[]) {
                 break;
         }
     }
-    // Close bt file
-    btcls(btfile);
 
     return 0;
 }
