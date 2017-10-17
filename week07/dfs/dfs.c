a02e328a4ab7f48293a5455a0796cb6c571af285
Do Hong Son <sonstephendo@gmail.com>
2017-10-17 15:14:10 +0700

    add new input, fix dfs alg

--- week07/dfs/dfs.c
+++ week07/dfs/dfs.c
@@ -2,31 +2,37 @@
 #include "dfs.h"
 #include "../../lib/Libfdr/dllist.h"
 #include "../../lib/Libfdr/jrb.h"
-#define SIZEMAX 100
 
 void DFS(Graph g, int start, int stop, void (*func)(int)) {
     Graph node;
     Graph visited = createGraph();
     Dllist stack;
     Dllist temp;
-    int adjacents[SIZEMAX];
+    
     int count;
     int i;
     int key;
 
-    node = jrb_find_int(g, start);
-    if (node == NULL) {
-        printf("Graph does not have vertex %d\n", start);
-        return;
-    }
-    
+    JRB check1 = jrb_find_int(g,start);
+	JRB check2 = jrb_find_int(g,stop);
+	if(check1 == NULL ) {
+		printf("Graph does not have vertex %d\n", start);
+		return;
+	}
+	if(stop != -1 && check2 == NULL) {
+		printf("Graph does not have vertex %d\n", stop);
+		return;
+	}
+
+    int V = 0;
     // Create a graph name 'visited' in which each node's key is
     // a vertex, and it's key indicate whether the vertex has been
     // visited (1) or not (0)
     jrb_traverse(node, g) {
+    	V++;
         jrb_insert_int(visited, jval_i(node->key), new_jval_i(0));
     }
-
+    int adjacents[V];
     // Create a stack and push the first element
     stack = new_dllist();
     dll_append(stack, new_jval_i(start));
@@ -37,22 +43,29 @@ void DFS(Graph g, int start, int stop, void (*func)(int)) {
         temp = dll_last(stack);
         key = jval_i(temp->val);
         
-        // Pass the vertex to external function
-        (*func)(key);
+        // Mark this element as 'visited'
+        node = jrb_find_int(visited, key);
+        if(jval_i(node->val) == 0) {
+        	// Pass the vertex to external function
+        	(*func)(key);
 
+        	// Mark this element as 'visited'
+        	jrb_delete_node(node);
+        	jrb_insert_int(visited, key, new_jval_i(1));
+
+        } else {
+        	// Pop this element
+        dll_delete_node(temp);
+        }
+        
+        
         // If this is the required vertex, return
         if (key == stop) {
+        	free_dllist(stack);
+			jrb_free_tree(visited);
             return;
         }
         
-        // Pop this element
-        dll_delete_node(temp);
-
-        // Mark this element as 'visited'
-        node = jrb_find_int(visited, key);
-        jrb_delete_node(node);
-        jrb_insert_int(visited, key, new_jval_i(1));
-
         count = getAdjacentVertices(g, key, adjacents);
         for (i = 0; i < count; i++) {
             // If the vertice has not been visited, push it
@@ -60,10 +73,9 @@ void DFS(Graph g, int start, int stop, void (*func)(int)) {
             if (jval_i(node->val) == 0) {
                 key = jval_i(node->key);
                 dll_append(stack, new_jval_i(key));
-                // Mark this element as 'visited'
-                jrb_delete_node(node);
-                jrb_insert_int(visited, key, new_jval_i(1));
             }
         }
     }
+    free_dllist(stack);
+	jrb_free_tree(visited);
 }
--- week07/dfs/main.c
+++ week07/dfs/main.c
@@ -3,29 +3,48 @@
 #include "dfs.h"
 
 void printVertex(int v) {
-    printf("%4d", v);
+    printf("%4c", v);
 }
 
 int main() {
     // 0___1___2___4___5
     //     |   |
     //     3___|
-    printf("Graph:\n");
-    printf("0__1__2__4__5\n");
-    printf("   |  |\n");
-    printf("   3__|\n");
+    // printf("Graph:\n");
+    // printf("0__1__2__4__5\n");
+    // printf("   |  |\n");
+    // printf("   3__|\n");
     Graph g = createGraph();
-    addEdge(g, 0, 1);
-    addEdge(g, 1, 2);
-    addEdge(g, 1, 3);
-    addEdge(g, 2, 3);
-    addEdge(g, 2, 4);
-    addEdge(g, 4, 5);
-    printf("\nBFS: start from node 1 to 4:\n");
-    DFS(g, 1, 4, printVertex);
-    printf("\nBFS: start from node 1 to all:\n");
-    DFS(g, 1, -1, printVertex);
-    printf("\n");
+    // addEdge(g, 0, 1);
+    // addEdge(g, 1, 2);
+    // addEdge(g, 1, 3);
+    // addEdge(g, 2, 3);
+    // addEdge(g, 2, 4);
+    // addEdge(g, 4, 5);
 
+
+    // printf("\nBFS: start from node 1 to 4:\n");
+    // DFS(g, 1, 4, printVertex);
+    // printf("\nBFS: start from node 1 to all:\n");
+    // DFS(g, 1, -1, printVertex);
+    // printf("\n");
+
+    addEdge(g, 'A', 'B');
+    addEdge(g, 'A', 'S');
+    addEdge(g, 'S', 'C');
+    addEdge(g, 'S', 'G');
+    addEdge(g, 'C', 'D');
+    addEdge(g, 'C', 'E');
+    addEdge(g, 'C', 'F');
+    addEdge(g, 'E', 'H');
+    addEdge(g, 'G', 'F');
+    addEdge(g, 'G', 'H');
+
+    printf("\nDFS: start from node 'A' to 'E':\n");
+    DFS(g, 'A', 'E', printVertex);
+    printf("\nDFS: start from node 'A' to all:\n");
+    DFS(g, 'A', -1, printVertex);
+
+    printf("\n");
     return 0;
 }
